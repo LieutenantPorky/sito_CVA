721a570b9383626936ba0e9d2e2274d0c291f76c
sinistraunita <parliamodiarte1@gmail.com>
2019-08-22 17:47:40 +0200

    roba

---
+++ CVA/polls/templates/polls/footer.php
@@ -0,0 +1,8 @@
+<div class="footer">
+ <hr>
+  <footer class="clearfix">
+   <p >Copyright - Leonardo Basso 2019 &copy;</p>
+  </footer>
+</div>
+</body>
+</html>
--- CVA/polls/templates/polls/index.php
+++ CVA/polls/templates/polls/index.php
@@ -1,23 +1,11 @@
    {% load static %}
 {% include 'polls/header.php' %}
-   <!--SideBar -->
-  <div class="sidebar">
-    <div class="sidebar__copy">
-      <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2988.0798275301395!2d12.578480814707708!3d41.502544879253776!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1325bcfe63317385%3A0xdd62b9e62cb98ec2!2sA.S.D.+Centro+Velico+L&#39;Atollo!5e0!3m2!1sit!2sit!4v1565890033497!5m2!1sit!2sit" allowfullscreen></iframe>
-    </div>
-  </div>
-
+{% include 'polls/sidebar.php' %}
   <!-- Slideshow container -->
 
   <div class="content clearfix">
    <img src="{% static 'polls/img/LogoFIV_CMYK.jpg'%}" alt="">
   </div>
   <br>
-  <div class="footer">
-   <hr>
-    <footer class="clearfix">
-     <p >Copyright - Leonardo Basso 2019 &copy;</p>
-    </footer>
-  </div>
- </body>
-</html>
+
+{% include 'polls/footer.php' %}
