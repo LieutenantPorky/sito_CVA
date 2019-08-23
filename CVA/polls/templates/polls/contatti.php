bc7052836bec844fbfd7bfa73052d3ae14c8d86d
sinistraunita <parliamodiarte1@gmail.com>
2019-08-23 18:01:32 +0200

    robaaaaaaaa

--- CVA/polls/templates/polls/contatti.php
+++ CVA/polls/templates/polls/contatti.php
@@ -21,8 +21,21 @@
     </center>
   </form>
     <!-- Instagram -->
-</div>
-  <br>
+    <div class="social">
+     <div class="instagram__conteiner">
+      <a href="https://www.instagram.com/cvlatolloofficial/" >
+      <img class="instaimg" src="{%static 'polls/img/instagram.jpg'%}" alt="">
+      </a>  <p class="instatext"> @cvlatolloofficial</p>
+     </div> <br> <br> <br> <br> <br>
+
+     <div class="facebook__conteiner">
+      <a href="https://www.instagram.com/cvlatolloofficial/" >
+      <img class="faceimg" src="{%static 'polls/img/facebook.png'%}" alt="">
+      </a>  <p class="facetext"> Centro Velico L'Atollo</p>
+     </div>
+    </div>
+   </div>
+
 
 
 
