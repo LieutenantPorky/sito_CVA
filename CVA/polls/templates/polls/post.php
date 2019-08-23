a9ec1f26231d3c7ef9e5474e8df67862f1da245d
lieutenantPorky <jacopo@siniscalco.eu>
2019-08-23 16:08:07 +0100

    added post view page

---
+++ CVA/polls/templates/polls/post.php
@@ -0,0 +1,22 @@
+{% load static %} <!-- Load Statics -->
+{% include 'polls/header.php' %} <!-- Header -->
+{% include 'polls/sidebar.php' %} <!-- SideBar -->
+  <!--Container -->
+
+  <div class="content clearfix">
+    <div class="articles">
+    {% if article %}
+    <h2> {{article.title}} </h2>
+    <p class="Desc"> {{article.body_text_1}} </p>
+    {% for image in article.articleimage_set.all%}
+    <img src="{{image.image.url}}" alt="">
+    <p>{{image.caption}}</p>
+    {% endfor %}
+  {% else %}
+      <p class="Contenuto">Nessuna pagina principale</p>
+  {% endif %}
+  </div>
+  </div>
+  <br>
+
+{% include 'polls/footer.php' %} <!-- Footer -->
