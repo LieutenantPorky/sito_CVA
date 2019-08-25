701e9815bfcf678cba63e16c00c16a2a632629d4
sinistraunita <parliamodiarte1@gmail.com>
2019-08-25 19:18:58 +0200

    robe

--- CVA/polls/static/polls/scripts.js
+++ CVA/polls/static/polls/scripts.js
@@ -15,5 +15,11 @@ $(document).ready(function() {
     e.preventDefault();
 
   });
+  jQuery('.scroll').click(function(e){
+      var jump = $(this).attr('href');
+      var new_position = $(jump).offset();
+      $('html, body').stop().animate({ scrollTop: new_position.top }, 500);
+      e.preventDefault();
+  });
 
 });
--- CVA/polls/templates/polls/index.php
+++ CVA/polls/templates/polls/index.php
@@ -17,15 +17,14 @@
 
     <h1>Centro Velico L'Atollo</h1>
     <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-    <center> <div class="cover__botton"><a href="#">Info</a></div> </center>
+    <center> <div class="cover__botton"><a href="#info"class="scroll">Info</a></div> </center>
   </div>
 </div>
 {% include 'polls/sidebar.php' %} <!-- SideBar -->
   <!--Container -->
-  <div class="content clearfix">
-
-
-
+  <div class="content clearfix" >
+  <div class="info" id="info"></div>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
   </div>
 
 {% include 'polls/footer.php' %} <!-- Footer -->
